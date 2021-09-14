aa4484fa5e5c9272fb86a38a8f3bbf1040abea59
Ilya Khrapko <iamdemos461@gmail.com>
2021-09-14 12:49:32 +0300

    completed homework-8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,37 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
+        case 'SORT': {
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.name < b.name ? 1 : -1)
+                : [...state].sort((a, b) => a.name > b.name ? 1 : -1)
         }
-        case 'check': {
-            // need to fix
-            return state
+        case 'CHECK': {
+            const copyState = [...state]
+            return copyState.filter(u => u.age > 18)
         }
-        default: return state
+        default:
+            return state
     }
 }
+
+type ActionType = sortACType | checkACType
+
+
+type sortACType = ReturnType<typeof sortAC>
+type checkACType = ReturnType<typeof checkAC>
+
+export const sortAC = (payload: 'up' | 'down') => {
+    return {
+        type: 'SORT',
+        payload
+    } as const
+}
+
+export const checkAC = () => {
+    return {
+        type: 'CHECK'
+    } as const
+}
+
